d3c1836dde342448263820b2f146c9ccbe0f1579
Hyunuk Jeong <67573511+hyunuk-Jeong@users.noreply.github.com>
2025-05-21 13:21:43 +0900

    remove back

--- api/server-time.js
+++ api/server-time.js
@@ -1,19 +1,16 @@
+// /api/server-time.js
 export default async function handler(req, res) {
   try {
-    const [res1, res2] = await Promise.all([
-      fetch('https://ticketlink.co.kr', { method: 'HEAD' }),
-      fetch('https://facility.ticketlink.co.kr', { method: 'HEAD' }),
-    ]);
+    const response = await fetch('https://ticketlink.co.kr', {
+      method: 'HEAD',
+    });
 
-    const date1 = res1.headers.get('date');
-    const date2 = res2.headers.get('date');
-
+    const dateHeader = response.headers.get('date');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Expose-Headers', 'date');
 
     res.status(200).json({
-      ticketlinkTime: date1,
-      facilityTime: date2,
+      serverTime: dateHeader,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
